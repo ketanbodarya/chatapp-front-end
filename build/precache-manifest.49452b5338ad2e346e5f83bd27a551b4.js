1fba728cfcd521115661241b7ba5430c2668c467
ketan-vedaha <ketan.b@vedaha.com>
2020-08-05 15:45:41 +0530

    initial commit

---
+++ build/precache-manifest.49452b5338ad2e346e5f83bd27a551b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fe3150e3c56c278ce4047d0c1dfd71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df3cc5cf887a3f110e5d",
+    "url": "/static/css/main.262bc6c1.chunk.css"
+  },
+  {
+    "revision": "19fd7e9ee32addaa8751",
+    "url": "/static/js/2.b5888177.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.b5888177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3cc5cf887a3f110e5d",
+    "url": "/static/js/main.3d66a347.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
